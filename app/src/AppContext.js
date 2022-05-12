891cf9d9f3c618a18a40cbb68694e9b79226f556
rahulshahaha <getrshah@gmail.com>
2022-05-12 11:06:32 -0400

    s

--- app/src/AppContext.js
+++ app/src/AppContext.js
@@ -51,7 +51,7 @@ export const AppProvider = ({ children }) => {
         {id:8,name: '', 'url':'https://berbix.latticehq.com/','logo':'latticehq.com'},
         {id:9,name: '', 'url':'https://todo-8303f.web.app/#/planning','logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Harp_%28887849%29_-_The_Noun_Project.svg/640px-Harp_%28887849%29_-_The_Noun_Project.svg.png',directLogo:true},
       ],
-      svg: <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+      svg: <svg xmlns="http://www.w3.org/2000/svg" className="" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
     </svg>
     }
